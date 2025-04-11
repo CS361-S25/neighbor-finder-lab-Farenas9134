b24eb159160bb1ca6c387eeb4fc210b32c1002d3
Farenas9134 <arenasf@carelton.edu>
2025-04-11 08:40:25 -0500

    WIP Lab

--- NFAnimate.cpp
+++ NFAnimate.cpp
@@ -4,11 +4,33 @@
 emp::web::Document doc{"target"};
 
 class NFAnimator : public emp::web::Animate {
+    emp::web::Canvas canvas{1000, 1000, "canvas"};
 
+    public:
+        NFAnimator() {
+            doc << canvas;
+
+            // creates 10 by 10 grid of "squares"
+            for (int i = 0; i < 10; ++i) {
+                for (int j = 0; j < 10; ++j) {
+                    int x = i * 30;
+                    int y = j * 30;
+                    canvas.Rect(x, y, 30, 30, "black", "white");
+                    canvas.CenterText(x + 15, y + 15, std::to_string(i)+","+std::to_string(j), "yellow", "yellow");
+                }
+            }
+        }
+
+        void FindNeighbors(int x, int y){
+            canvas.Rect(x * 30, y * 30, 30, 30, "yellow", "white");
+            // Need to find 8 neighbors around cell
+            
+        }
 };
 
 NFAnimator animator;
 
 int main() {
     animator.Step();
+    animator.FindNeighbors(5, 5);
 }
